81e572b2932d6c19d133e1119e54b244c1b137d2
Stepbackru <alex-ruba@bk.ru>
2021-08-05 23:29:32 +0300

    feat: add basic structure of filterComments

---
+++ src/components/filterComments.js
@@ -0,0 +1,22 @@
+const FilterComments = (props) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const today = `${year}-${addZero(month)}-${addZero(day)}`;
+
+  return (
+  <div className="filter-comments">
+    <p className="filter-comments__title">Сортировать:</p>
+    <select className="filter-comments__choice">
+      <option value={`created_at:lt:${today}`}>Новые</option>
+      <option value={`created_at:gt:${today}`}>Старые</option>
+    </select>
+  </div>);
+}
+
+const addZero = (n) => {
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+export default FilterComments;
