86c69096f9be0d111cde9210b8164c31c184a34c
Stepbackru <alex-ruba@bk.ru>
2021-08-06 20:33:09 +0300

    fix: change props rating for RatingStars

--- src/components/comments/headerComment.js
+++ src/components/comments/headerComment.js
@@ -3,7 +3,7 @@ import RatingStars from '../ratingStars';
 
 const HeaderComment = (props) => {
   let timeUsage = null;
-  
+
   if (props.data.author.details) {
     timeUsage = props.data.author.details
       .find((el) => el.label === 'Срок использования');
@@ -13,7 +13,7 @@ const HeaderComment = (props) => {
     <header className="comment__header">
       <div className="author">
         <p className="author__name">{props.data.author.name}</p>
-        <RatingStars rating={props.data.author.rating} countStars={props.countStars}/>
+        <RatingStars rating={props.data.rating} countStars={props.countStars}/>
         <p className="author__detail">
           {timeUsage ? `${timeUsage.label}: ${timeUsage.value}` : null}
         </p>
