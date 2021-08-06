ad40d9a5506158bf309c0dde88a94a83a5d2184a
Stepbackru <alex-ruba@bk.ru>
2021-08-06 20:31:35 +0300

    feat: add HeaderComment to comment componen

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,12 +1,14 @@
+import HeaderComment from "./headerComment";
+
 const Comments = (props) => {
 
   return (
     <section className="comment">
       {
-        props.reviews.map((el, ind) => {
+        props.data.reviews.map((el, ind) => {
           return (
             <div className="comment__item">
-              
+              <HeaderComment data={el} countStars={props.countStars}/>
             </div>
           );
         })
