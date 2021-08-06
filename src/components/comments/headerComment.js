ca613434670ada58b1f44c7c72b3ea5a375b1ba0
Stepbackru <alex-ruba@bk.ru>
2021-08-06 20:32:08 +0300

    feat: add logic of render timeUsage of user

--- src/components/comments/headerComment.js
+++ src/components/comments/headerComment.js
@@ -2,6 +2,12 @@ import RatingStars from '../ratingStars';
 
 
 const HeaderComment = (props) => {
+  let timeUsage = null;
+  
+  if (props.data.author.details) {
+    timeUsage = props.data.author.details
+      .find((el) => el.label === 'Срок использования');
+  }
 
   return (
     <header className="comment__header">
@@ -9,7 +15,7 @@ const HeaderComment = (props) => {
         <p className="author__name">{props.data.author.name}</p>
         <RatingStars rating={props.data.author.rating} countStars={props.countStars}/>
         <p className="author__detail">
-          {`Срок использования: ${props.data.author.details[1].value}`}
+          {timeUsage ? `${timeUsage.label}: ${timeUsage.value}` : null}
         </p>
       </div>
       <p className="comment__date">${props.data.created_at}</p>
