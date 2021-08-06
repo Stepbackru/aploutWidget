58bd3fd0132277022270498a5982b66b23873f5b
Stepbackru <alex-ruba@bk.ru>
2021-08-06 21:13:01 +0300

    feat: add buttons like&dislike for comments

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -5,11 +5,15 @@ const Comments = (props) => {
   return (
     <section className="comment">
       {
-        props.data.reviews.map((el, ind) => {
+        props.data.reviews.map((el) => {
           return (
-            <div className="comment__item">
+            <div key={el.id} className="comment__item">
               <HeaderComment data={el} countStars={props.countStars}/>
               <p className="comment__review">{el.body}</p>
+              <div className="comment_btns">
+                <button className="comment__btn comment__btn_like"></button>
+                <button className="comment__btn comment__btn_dislike"></button>
+              </div>
             </div>
           );
         })
