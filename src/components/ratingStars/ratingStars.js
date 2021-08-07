01093a436396b27635ea27753eaf2482f8735a9a
Stepbackru <alex-ruba@bk.ru>
2021-08-07 12:34:47 +0300

    fix: name of class

--- src/components/ratingStars/ratingStars.js
+++ src/components/ratingStars/ratingStars.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 const RatingStars = (props) => {
   const stars = Array(props.countStars).fill(0);
   const count = Math.ceil(props.rating);
@@ -6,10 +8,10 @@ const RatingStars = (props) => {
   <div className="rating">
     <div className="stars">
       {stars.map((_, ind) => {
-        const isStarOn = ind < count ? 'stars__item_on' : '';
+        const isStarOn = ind < count ? 'stars__item_on' : 'stars__item_off';
 
         if (ind + 1 === count && !Number.isInteger(props.rating)) {
-          return <div key={ind} className={`stars__item stars__item-half`}></div>
+          return <div key={ind} className={`stars__item stars__item_half`}></div>
         } else {
           return <div key={ind} className={`stars__item ${isStarOn}`}></div>
         }
